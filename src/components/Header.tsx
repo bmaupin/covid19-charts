9024a17d2d87a35ca15d88fa6845ed24d146fabe
bmaupin <bmaupin@users.noreply.github.com>
2020-03-19 13:03:37 -0400

    Remove demo code and add simple header bar

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Alignment, Button, Navbar } from '@blueprintjs/core';
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar className="bp3-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          {/* TODO: Update title */}
+          <Navbar.Heading>React App</Navbar.Heading>
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+          <a href="https://github.com/bmaupin/ts-react-wip">
+            {/* TODO: Use Github icon? */}
+            <Button className="bp3-minimal" icon="git-repo" />
+          </a>
+        </Navbar.Group>
+      </Navbar>
+    </header>
+  );
+}
