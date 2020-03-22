4c98414d0b25bb0135d5cec947bdc3678f34ea48
bmaupin <bmaupin@users.noreply.github.com>
2020-03-22 14:04:58 -0400

    Auto commit from gh-pages [ci skip]

---
+++ precache-manifest.839b09e6fca67cbc00a5e99b4b8c8a76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc8b37bd0c42adba8c0ff735f2b0ddd",
+    "url": "/covid19-charts/index.html"
+  },
+  {
+    "revision": "f7a2d5a8d818cd921bff",
+    "url": "/covid19-charts/static/css/2.7195bffc.chunk.css"
+  },
+  {
+    "revision": "d834026fdd591dad545a",
+    "url": "/covid19-charts/static/css/main.c12d1ad7.chunk.css"
+  },
+  {
+    "revision": "f7a2d5a8d818cd921bff",
+    "url": "/covid19-charts/static/js/2.24d81e00.chunk.js"
+  },
+  {
+    "revision": "37977ae1568a24ca13d29ee7cd3bf94f",
+    "url": "/covid19-charts/static/js/2.24d81e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d834026fdd591dad545a",
+    "url": "/covid19-charts/static/js/main.d6c1602f.chunk.js"
+  },
+  {
+    "revision": "6297918e8e136a18e94f",
+    "url": "/covid19-charts/static/js/runtime-main.ae1f0089.js"
+  }
+]);
