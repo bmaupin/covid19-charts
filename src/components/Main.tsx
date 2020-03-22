18817181d1de6408c95eb597806870748d5acc71
bmaupin <bmaupin@users.noreply.github.com>
2020-03-22 12:56:19 -0400

    Add button group to change chart metric

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Main.css';
 import Chart from './Chart';
+import { Button, ButtonGroup } from '@blueprintjs/core';
+import { ChartMetrics } from '../helpers/ChartDataHelper';
 
 function Main() {
+  const [chartMetric, setChartMetric] = useState(ChartMetrics.Confirmed);
+
+  function handleChartMetricButtonClick(newMetric: ChartMetrics) {
+    setChartMetric(newMetric);
+  }
+
   return (
     <div className="Main">
-      <Chart />
+      <ButtonGroup className="ButtonGroup">
+        <Button
+          active={chartMetric === ChartMetrics.Confirmed}
+          onClick={() => handleChartMetricButtonClick(ChartMetrics.Confirmed)}
+        >
+          Confirmed
+        </Button>
+        <Button
+          active={chartMetric === ChartMetrics.Deaths}
+          onClick={() => handleChartMetricButtonClick(ChartMetrics.Deaths)}
+        >
+          Deaths
+        </Button>
+        <Button
+          active={chartMetric === ChartMetrics.Recovered}
+          onClick={() => handleChartMetricButtonClick(ChartMetrics.Recovered)}
+        >
+          Recovered
+        </Button>
+      </ButtonGroup>
+      <Chart chartMetric={chartMetric} />
     </div>
   );
 }
