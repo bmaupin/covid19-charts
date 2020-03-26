3c74e7c4fa660f66c3a3e206b52995aa6c86695c
bmaupin <bmaupin@users.noreply.github.com>
2020-03-25 20:55:09 -0400

    Switch to pint-based hint

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import {
   FlexibleWidthXYPlot,
+  Hint,
   HorizontalGridLines,
   LineMarkSeries,
   VerticalGridLines,
   XAxis,
   YAxis,
-  Crosshair,
 } from 'react-vis';
 
 import '../../node_modules/react-vis/dist/style.css';
@@ -25,20 +25,31 @@ interface IProps {
 
 function Chart(props: IProps) {
   const [chartData, setChartData] = useState<ChartData | null>(null);
-  const [crosshairValues, setCrossHairValues] = useState<any[] | null>(null);
+  const [hintValue, setHintValue] = useState<any | null>(null);
+  const [hoveredSeriesIndex, setHoveredSeriesIndex] = useState<number | null>(
+    null
+  );
   const [isDataLoaded, setIsDataLoaded] = useState(false);
 
-  /**
-   * Event handler for chart onNearestX.
-   *
-   * This will set the crosshair values based on the x coordinate closest to the user's mouse.
-   * @param {Object} value Selected value.
-   * @param {number} index Index of the series.
-   * @private
-   */
-  const _onNearestX = (value: any, { index }: any) => {
-    console.log('_onNearestX');
-    setCrossHairValues(chartData!.map(entry => entry.data[index]));
+  const _formatHint = (value: any) => {
+    console.log('_formatHint value: ', value);
+
+    return [
+      {
+        title: chartData![hoveredSeriesIndex!].title,
+        value: value.y,
+      },
+    ];
+  };
+
+  const _onValueMouseOut = () => {
+    setHintValue(null);
+    setHoveredSeriesIndex(null);
+  };
+
+  const _onValueMouseOver = (value: any, index: number) => {
+    setHintValue(value);
+    setHoveredSeriesIndex(index);
   };
 
   const _xAxisLabelFormatter = (tick: number): string => {
@@ -95,16 +106,17 @@ function Chart(props: IProps) {
             tickTotal={chartData![0].data.length}
           />
           <YAxis tickFormat={_yAxisLabelFormatter} />
-          {chartData!.map((entry: any) => (
+          {chartData!.map((entry: any, i: number) => (
             <LineMarkSeries
               data={entry.data}
               // Don't render null y values; by default react-vis will convert them to 0
               getNull={d => d.y !== null}
               key={entry.title}
-              onNearestX={_onNearestX}
+              onValueMouseOut={_onValueMouseOut}
+              onValueMouseOver={datapoint => _onValueMouseOver(datapoint, i)}
             />
           ))}
-          <Crosshair values={crosshairValues!} />
+          {hintValue && <Hint format={_formatHint} value={hintValue} />}
         </FlexibleWidthXYPlot>
       )}
     </div>
