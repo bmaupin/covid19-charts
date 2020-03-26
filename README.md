40bf7d7fc5b4606a24eb2766f0076e54f1f615b4
bmaupin <bmaupin@users.noreply.github.com>
2020-03-25 21:23:47 -0400

    Switch back to react-charts from react-vis
    Neither one is perfect but it seems it's going to be a lot easier to add a legend to react-charts than to get react-vis how we want it
    
    See 18185a21

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Simple charts showing COVID-19 trends
 - [TypeScript](https://www.typescriptlang.org/)
 - [React](https://reactjs.org/)
 - [Blueprint](https://blueprintjs.com/)
-- [React-vis](https://uber.github.io/react-vis/)
+- https://github.com/tannerlinsley/react-charts
 - https://github.com/pomber/covid19
--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,17 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {
-  FlexibleWidthXYPlot,
-  Hint,
-  HorizontalGridLines,
-  LineMarkSeries,
-  VerticalGridLines,
-  XAxis,
-  YAxis,
-} from 'react-vis';
-
-import '../../node_modules/react-vis/dist/style.css';
-
-import './Chart.css';
+import { Chart as ReactChart } from 'react-charts';
 import {
   ChartMetrics,
   ChartDataHelper,
@@ -24,51 +12,13 @@ interface IProps {
 }
 
 function Chart(props: IProps) {
+  const axes = [
+    { primary: true, type: 'time', position: 'bottom' },
+    { type: 'linear', position: 'left' },
+  ];
+
   const [chartData, setChartData] = useState<ChartData | null>(null);
-  const [hintValue, setHintValue] = useState<any | null>(null);
-  const [hoveredSeriesIndex, setHoveredSeriesIndex] = useState<number | null>(
-    null
-  );
   const [isDataLoaded, setIsDataLoaded] = useState(false);
-
-  const _formatHint = (value: any) => {
-    console.log('_formatHint value: ', value);
-
-    return [
-      {
-        title: chartData![hoveredSeriesIndex!].title,
-        value: value.y,
-      },
-    ];
-  };
-
-  const _onValueMouseOut = () => {
-    setHintValue(null);
-    setHoveredSeriesIndex(null);
-  };
-
-  const _onValueMouseOver = (value: any, index: number) => {
-    setHintValue(value);
-    setHoveredSeriesIndex(index);
-  };
-
-  const _xAxisLabelFormatter = (tick: number): string => {
-    return new Date(chartData![0].dates[tick]).toLocaleString('default', {
-      month: 'short',
-      day: 'numeric',
-    });
-  };
-
-  const _yAxisLabelFormatter = (tick: number): string => {
-    // Pray we never need this...
-    if (chartData![0].data[chartData![0].data.length - 1].y > 10000000) {
-      return String(tick / 1000000) + 'm';
-    } else if (chartData![0].data[chartData![0].data.length - 1].y > 10000) {
-      return String(tick / 1000) + 'k';
-    } else {
-      return String(tick);
-    }
-  };
 
   useEffect(() => {
     async function loadChartData() {
@@ -87,38 +37,16 @@ function Chart(props: IProps) {
   }, [props.chartMetric]);
 
   return (
-    <div className="ChartContainer">
-      {/* Don't render the chart until the data is loaded */}
-      {isDataLoaded && (
-        <FlexibleWidthXYPlot
-          height={600}
-          // Prevent tick labels from being cut off (https://github.com/uber/react-vis/issues/400)
-          margin={{
-            left: 40,
-            right: 40,
-          }}
-        >
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <XAxis
-            tickFormat={_xAxisLabelFormatter}
-            // Only one tick per date (without this react-vis may create ticks and labels between dates)
-            tickTotal={chartData![0].data.length}
-          />
-          <YAxis tickFormat={_yAxisLabelFormatter} />
-          {chartData!.map((entry: any, i: number) => (
-            <LineMarkSeries
-              data={entry.data}
-              // Don't render null y values; by default react-vis will convert them to 0
-              getNull={d => d.y !== null}
-              key={entry.title}
-              onValueMouseOut={_onValueMouseOut}
-              onValueMouseOver={datapoint => _onValueMouseOver(datapoint, i)}
-            />
-          ))}
-          {hintValue && <Hint format={_formatHint} value={hintValue} />}
-        </FlexibleWidthXYPlot>
-      )}
+    // A react-chart hyper-responsively and continuously fills the available
+    // space of its parent element automatically
+    <div
+      style={{
+        width: '800px',
+        height: '600px',
+      }}
+    >
+      {/* The axes won't show correctly if the chart is rendered before the data is loaded */}
+      {isDataLoaded && <ReactChart data={chartData} axes={axes} tooltip />}
     </div>
   );
 }
